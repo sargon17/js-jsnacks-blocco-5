546d234fb65851389747a98037162b43d7600338
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-22 18:50:48 +0100

    Fifth exercise done

--- snack-5/script.js
+++ snack-5/script.js
@@ -1,1 +1,11 @@
+let array = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+function arraySection(array, min, max) {
+  let newArray = [];
+  for (let index = ++min; index < max; index++) {
+    newArray.push(array[index]);
+  }
+  return newArray;
+}
+
+console.log(arraySection(array, 1, 5));
