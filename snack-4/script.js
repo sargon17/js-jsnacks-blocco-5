d1b2e57d0b8521f3f3e772748008ada247d4d6e8
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-22 18:42:12 +0100

    Exercise 4 done

--- snack-4/script.js
+++ snack-4/script.js
@@ -1,1 +1,20 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di elementi) prendendo alternativamente gli elementi da uno e dall’altro, es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
 
+let numbers = [1, 2, 3, 4, 5];
+let letters = ["a", "b", "c", "d", "e"];
+
+console.log(mergeTwoArrayInOrder(numbers, letters));
+
+function mergeTwoArrayInOrder(array1, array2) {
+  let array3 = [];
+  console.log(array1, array2);
+  while (array1.length > 0 && array2.length > 0) {
+    let [element1, ...args1] = array1;
+    array1 = args1;
+    let [element2, ...args2] = array2;
+    array2 = args2;
+
+    array3 = [...array3, element1, element2];
+  }
+  return array3;
+}
