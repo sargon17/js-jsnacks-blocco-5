39d57510eef4060730f4a54b77d6c0d36c0f8f07
Mykhaylo (Michael) Tymofyeyev <mykhaylo.tymofyeyev@gmail.com>
2022-03-22 17:56:23 +0100

    third exercise done

--- snack-3/script.js
+++ snack-3/script.js
@@ -1,1 +1,9 @@
+// Scrivi una funzione che accetti una stringa come argomento e la ritorni girata (es. Ciao -> oaiC)
 
+function mirrorWord(string) {
+  let splitReverseString = string.split("").reverse();
+  let newString = splitReverseString.join("");
+  return newString;
+}
+
+console.log(mirrorWord("ciao"));
